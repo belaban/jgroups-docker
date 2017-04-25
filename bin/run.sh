e983db639a887c9e2a73a81bd77b6f52cb3ed4ae
Bela Ban <belaban@yahoo.com>
2017-04-25 09:39:19 +0200

    ns

--- bin/run.sh
+++ bin/run.sh
@@ -15,6 +15,7 @@ fi;
 
 
 FLAGS="-Djava.net.preferIPv4Stack=true -server -Xmx1G -Xms500M -XX:+UseG1GC"
+DEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8787"
 
 props=udp.xml
 #aws=false
