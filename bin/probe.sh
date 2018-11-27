56af677bcc50c5fda49cabd71e208c4a5072ef79
Bela Ban <belaban@yahoo.com>
2018-11-27 11:08:01 +0100

    ns

--- bin/probe.sh
+++ bin/probe.sh
@@ -3,5 +3,6 @@
 # Discovers all UDP-based members running on a certain mcast address (use -help for help)
 # Probe [-help] [-addr <addr>] [-port <port>] [-ttl <ttl>] [-timeout <timeout>]
 
+CP=$HOME/jgroups-docker/lib/jgroups-4*.jar
 
-`dirname $0`/run.sh org.jgroups.tests.Probe $*
+java -cp $CP org.jgroups.tests.Probe $*
